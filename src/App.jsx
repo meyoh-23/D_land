3aa4c80ae1028fe15fb24b9beb956b802fb6594d
Felix Ouma <moxiemeyoh@gmail.com>
2023-12-28 17:47:35 +0300

    Add: Routing - implemented Routing - navigation to the home page from the routing library used

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import {Routes, Route} from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
 
   return (
     <>
+    <Header/>
       <Routes>
         <Route path="/" element={<Login/>}/>
+        <Route path="/home" element={<Home/>}/>
       </Routes>
     </>
   )
 }
 
-export default App
+export default App;
